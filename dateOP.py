c26984135e7c6e5628c3491729fcb6f384ce6588
nora <nora_abdullah33@outlook.com>
2022-07-04 20:02:17 +0300

    lap 5

---
+++ dateOP.py
@@ -0,0 +1,7 @@
+
+import datetime 
+
+
+def printDATE()->None :
+    todays_date = datetime.date.today()
+    print("todays date is " , todays_date ) 
---
+++ main.py
@@ -0,0 +1,29 @@
+#q1 
+from tkinter import Y
+import dateOP 
+from dateOP import printDATE 
+printDATE()
+
+#q2 
+
+ 
+def additoin(x, y):
+          x = 10
+          y = 20
+          
+          print("Addition:", x + b)
+
+
+
+try : 
+   additoin(10, 20) 
+
+except : 
+    print("'b' is not defined")
+
+else : 
+    print(" the operation is successful")
+
+
+
+
